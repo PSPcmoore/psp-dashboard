04eb103aff19d2375c53f55439a872817c351873
PSPcmoore <cmoore@petersonpartners.com>
2026-04-15 16:35:42 -0600

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,19 @@
+javascriptexport default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  if (req.method === 'OPTIONS') { res.status(200).end(); return; }
+
+  const affinityUrl = 'https://api.affinity.co' + req.url.replace('/api/proxy', '');
+  const apiKey = req.headers['x-affinity-key'];
+
+  const response = await fetch(affinityUrl, {
+    headers: {
+      'Authorization': 'Bearer ' + apiKey,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
